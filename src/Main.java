4979aed89630604207c325cb3367b7a1dfad327e
unknown <ruslanovich.ik@gmail.com>
2021-09-26 17:57:18 +0200

    buttons working slowly

--- src/Main.java
+++ src/Main.java
@@ -6,15 +6,15 @@ import java.awt.event.KeyEvent;
 public class Main extends JFrame {
     Field field = new Field();
     DrawPanel panel = new DrawPanel();
-
+    int n = 1;
     public Main() {
         panel.setBackground(Color.black);
         panel.setVisible(true);
-        add(panel);                                          //adds panel to JFrame!!!
-        addKeyListener(new DrawPanelKeyListener());
+        add(panel);                                             //adds panel to JFrame!!!
+        addKeyListener(new DrawPanelKeyListener());             //also, we should add DrawPanelKeylistener() class.
     }
     public static void main(String[] args) {
-        Main app = new Main();                               // here codes are creating my JFrame components, characteristics.
+        Main app = new Main();                                  // here codes are creating my JFrame components, characteristics.
         app.setSize(900,700);
         app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         app.setVisible(true);
@@ -24,6 +24,8 @@ public class Main extends JFrame {
             super.paintComponent(g);
             g.setColor(Color.blue);
             g.setFont(g.getFont().deriveFont(50f));
+            String sc = Integer.toString(field.score);
+            g.drawString(sc, 900, 50);
             g.drawString("Snake 1.1", 600, 50);
             for (int j = 0; j < 40; j++) {
                 for (int i = 0; i < 20; i++) {
@@ -47,14 +49,21 @@ public class Main extends JFrame {
                     }
                 }
             }
-            if (field.dir == 0) {
-                field.moveUp();
-            }if (field.dir == 1) {
-                field.moveRight();
-            }if (field.dir == 2) {
-                field.moveDown();
-            }if (field.dir == 3) {
-                field.moveLeft();
+            switch (n) {
+                case 0:
+                    field.moveUp();
+                    break;
+                case 1:
+                    field.moveRight();
+                    break;
+                case 2:
+                    field.moveDown();
+                    break;
+                case 3:
+                    field.moveLeft();
+                    break;
+                default:
+                    break;
             }
             repaint();
         }
@@ -64,20 +73,37 @@ public class Main extends JFrame {
             int KeyCode = e.getKeyCode();
             switch (KeyCode) {
                 case KeyEvent.VK_UP:
-                    field.moveUp();
+                    if (field.dir != 2) {
+                        n = 0;
+                    }else {
+                        n = field.dir;
+                    }
                     break;
                 case KeyEvent.VK_DOWN:
-                    field.moveDown();
+                    if (field.dir != 0) {
+                        n = 2;
+                    }else {
+                        n = field.dir;
+                    }
                     break;
                 case KeyEvent.VK_LEFT:
-                    field.moveLeft();
+                    if (field.dir != 1) {
+                        n = 3;
+                    }else {
+                        n = field.dir;
+                    }
                     break;
                 case KeyEvent.VK_RIGHT:
-                    field.moveRight();
+                    if (field.dir != 3) {
+                        n = 1;
+                    }else {
+                        n = field.dir;
+                    }
                     break;
                 default:
                     break;
             }
+            repaint();
         }
     }
 }
